8b0ff9bb90b4a5849c58f53a8a8258a6cedbe2b2
cal250 <jeancalvin027@gmail.com>
2024-07-03 19:02:00 +0200

    initialization

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget{
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext Context) {
+  Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: WeatherPage(),
